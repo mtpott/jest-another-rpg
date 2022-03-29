9234de9132510f36f97687c3f326120e228b3e1b
megan <mtpott23@gmail.com>
2022-03-29 11:56:59 -0400

    prototypes for getStats and getInventory

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,9 +1,11 @@
 //const { expect, jest } = require('@jest/globals');
+//const { expect } = require('@jest/globals');
+//const { test } = require('picomatch');
 const Player = require('../lib/Player');
 
 const Potion = require('../lib/Potion');
 
-jest.mock('../lib/Potion.js');
+//jest.mock('../lib/Potion');
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -16,4 +18,23 @@ test('creates a player object', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
+});
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 });
